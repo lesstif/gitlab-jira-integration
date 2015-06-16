d8789047a965ba3eca05a59c7d246ddeb8cddc94
KwangSeob Jeong <lesstif@gmail.com>
2015-06-17 00:38:32 +0900

    add route

--- index.php
+++ index.php
@@ -4,8 +4,16 @@ require 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
 
+$app->get('/', function () {
+    echo "Hello";
+});
+
 $app->get('/hello/:name', function ($name) {
     echo "Hello, $name";
+});
+
+$app->post('/gitlab/', function () {    
+    $app->response->setStatus(503);
 });
 
 $app->run();
